766c0b67db462a6b4a5871b88e2b35bdbfe59103
KiranKKhehra <116680883+KiranKKhehra@users.noreply.github.com>
2022-10-26 18:17:22 -0400

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,29 @@
+package jul22.week1.day1.assignments;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int Input= 13;
+		boolean flag= false;
+		for (int i=2; i<=Input/2;i++) {
+				if(Input%2==0) {
+					flag=true;
+					break;
+				}	
+				}
+		if (flag==false)
+		
+		System.out.println("Prime number=13");
+		
+		 else
+		    
+		    	System.out.println("Not a Prime number");
+		    
+		
+			
+			
+
+	}
+
+}
+
